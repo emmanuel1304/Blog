6f6ce70f311032cc0d15c37ce9d692b374b58908
lucky okirie <okirielucky1304@gmail.com>
2023-03-07 10:35:58 -0600

    changes

--- blogapp/models.py
+++ blogapp/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from ckeditor_uploader.fields import RichTextUploadingField #import this
 from django.urls import reverse
 
 # post model
@@ -25,7 +26,7 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
-    body = models.TextField()
+    body=RichTextUploadingField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     categories = models.ForeignKey(Catagory,on_delete=models.DO_NOTHING, null=True)
@@ -46,3 +47,23 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('blog:post_detail',args=[self.slug])
 
+    def get_comments(self):
+        return Comment.objects.filter(parent=self).filter(active=True)
+
+class Comment(models.Model):
+    post=models.ForeignKey(Post,on_delete=models.CASCADE, related_name="comments")
+    name=models.CharField(max_length=50)
+    email=models.EmailField()
+    parent=models.ForeignKey("self", null=True, blank=True, on_delete=models.CASCADE)
+    body = models.TextField()
+    
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    class Meta:
+        ordering = ('created',)
+    
+    def __str__(self):
+        return self.body
+    def get_comments(self):
+        return Comment.objects.filter(parent=self).filter(active=True)
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,28 +1,42 @@
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 # Create your views here.
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage 
 from django.shortcuts import render
-from .models import Post, Catagory
+from .models import Post, Catagory, Comment
+from .forms import CommentForm
 
 def post_list(request):
     posts = Post.objects.filter(status='published')
-    paginator = Paginator(posts, 1) # 10 posts in each page
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range deliver last page of results
-        posts = paginator.page(paginator.num_pages)
+    
     categories = Catagory.objects.all()
     featured = Post.objects.filter(featured=True)
     breaking_news = Post.objects.filter(breaking_news=True)
-    return render(request,'blogapp/index.html',context={'posts':posts, 'categories': categories, 'featured': featured, 'breaking_news': breaking_news, 'page':page})
+    return render(request,'blogapp/index.html',context={'posts':posts, 'categories': categories, 'featured': featured, 'breaking_news': breaking_news,})
 
 
 def post_detail(request, id):
     post=Post.objects.get(id=id)
-    return render(request, 'blogapp/blog-detail.html',context={'post':post})    
+    
+    return render(request, 'blogapp/blog-detail.html',context={'post':post,})    
+
+# handling reply, reply view
+def reply_page(request):
+    if request.method == "POST":
+
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            post_id = request.POST.get('post_id')  # from hidden input
+            parent_id = request.POST.get('parent')  # from hidden input
+            post_url = request.POST.get('post_url')  # from hidden input
+
+            reply = form.save(commit=False)
+    
+            reply.post = Post(id=post_id)
+            reply.parent = Comment(id=parent_id)
+            reply.save()
+
+            return redirect(post_url+'#'+str(reply.id))
+
+    return redirect("/")    
